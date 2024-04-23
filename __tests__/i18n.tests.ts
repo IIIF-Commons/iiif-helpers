d39b265e6502d3484020bb81210e1bfc90c90c3f
Stephen Fraser <stephen.fraser@digirati.com>
2024-04-23 18:31:38 +0100

    Added getAvailableLanguagesFromResource() helper to i18n helpers

--- __tests__/i18n.tests.ts
+++ __tests__/i18n.tests.ts
@@ -1,8 +1,10 @@
 /**
  * @jest-environment node
  */
-import { buildLocaleString, getValue } from '../src/i18n';
+import { buildLocaleString, getValue, getAvailableLanguagesFromResource } from '../src/i18n';
 import { describe, test, expect } from 'vitest';
+import delftExample from '../fixtures/exhibitions/novieten.json';
+import compositeTest from '../fixtures/cookbook/composite.json';
 
 describe('i18n helper', () => {
   describe('buildLocaleString()', () => {
@@ -57,6 +59,50 @@ describe('i18n helper', () => {
       expect(buildLocaleString({ none: ['A', 'B'] }, 'none', { separator: '' })).toEqual('AB');
       expect(buildLocaleString({ none: ['A', 'B'] }, 'none', { separator: '<br/>' })).toEqual('A<br/>B');
       expect(buildLocaleString({ none: ['A', 'B', 'C', 'D', 'E'] }, 'none', { separator: ' ' })).toEqual('A B C D E');
+    });
+  });
+
+  describe('getAvailableLanguagesFromResource()', () => {
+    test('it can get languages from a resource', () => {
+      const found = getAvailableLanguagesFromResource({
+        id: 'http://example.com/iiif/book1/canvas/p1',
+        type: 'Canvas',
+        label: { en: ['p. 1'], fr: ['p. 1'] },
+        height: 1000,
+        width: 750,
+      });
+      expect(found).toHaveLength(2);
+      expect(found).toContain('en');
+      expect(found).toContain('fr');
+    });
+
+    test('it can get languages from a resource with nested values', () => {
+      const found = getAvailableLanguagesFromResource(delftExample as any);
+      expect(found).toHaveLength(2);
+      expect(found).toContain('en');
+      expect(found).toContain('nl');
+    });
+
+    test('it will skip none languages', () => {
+      const found = getAvailableLanguagesFromResource({
+        id: 'http://example.com/iiif/book1/canvas/p1',
+        type: 'Canvas',
+        label: { none: ['p. 1'], fr: ['p. 1'] },
+        height: 1000,
+        width: 750,
+      });
+
+      expect(found).toHaveLength(1);
+      expect(found).not.toContain('none');
+      expect(found).toContain('fr');
+    });
+
+    test('it can get languages from a composite resource', () => {
+      const found = getAvailableLanguagesFromResource(compositeTest as any);
+
+      expect(found).toHaveLength(2);
+      expect(found).toContain('en');
+      expect(found).toContain('fr');
     });
   });
 });
