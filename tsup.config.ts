9e379bed3e354afd61ebbae72494fec3dcd59c85
Stephen Fraser <stephen.fraser@digirati.com>
2024-05-25 18:02:55 +0100

    Search1 fixes

--- tsup.config.ts
+++ tsup.config.ts
@@ -23,6 +23,7 @@ export default defineConfig((options: Options) => ({
     'vault-store': 'src/vault-store.ts',
     'vault-actions': 'src/vault-actions.ts',
     'vault-utility': 'src/vault-utility.ts',
+    search1: 'src/search1.ts',
     // New helpers can be added here.
   },
   minify: false,
